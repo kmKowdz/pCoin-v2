6d9fee5f8c274861ea82d756cfb6d470e9f55263
KmKowdz <kmalarcon@live.com>
2020-09-07 10:35:52 +0800

    update blockchain.service.ts
    - imported the blockchain and elliptic
    - created a blockchain instance
    - declared a variable to store the walletkeys
    - instantiated some values
    - added a method to retrieve blocks
    - added a method to generate wallet keys

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+//import blockchain module
+import { Blockchain } from 'pCoin/src/blockchain';
+//import elliptic library to generate key pair
+import EC from "elliptic";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
 
-  constructor() { }
+  //generate an instance of a blockchain
+  public blockchainInstance = new Blockchain();
+  //create a variable to store the walletkeys
+  public walletKeys = []; //array for multiple key pairs
+
+  //instantiate the difficulty level, mine pending transactions and generate wallet keys as this service is run
+  constructor() { 
+    this.blockchainInstance.difficulty = 1;
+    this.blockchainInstance.minePendingTransactions('my-wallet-address');
+  
+    this.generateWalletKeys();
+  }
+
+  //add a method to retrieve blocks
+  getBlocks(){
+    return this.blockchainInstance.chain;
+  }
+
+  //add a method to generate wallet keys
+  private generateWalletKeys(){
+    const ec = new EC.ec('secp256k1');
+    const key = ec.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj: key,
+      publicKey: key.getPublic('hex'),
+      privateKey: key.getPrivate('hex'),
+    });
+  }
 }
