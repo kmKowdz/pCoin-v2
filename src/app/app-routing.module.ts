e19ddd59cc96196a6e4796c8d448faa457b742ad
KmKowdz <kmalarcon@live.com>
2020-09-07 12:21:15 +0800

    update app-routing.module.ts
    - imported blockchain viewer component
    - added path to locate the viewer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import the blockchain viewer component
+import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BlockchainViewerComponent } //add path where the component will be located
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
