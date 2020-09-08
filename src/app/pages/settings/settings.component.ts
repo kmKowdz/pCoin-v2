870df5cda24a329e2943604741bf34d45340098f
KmKowdz <kmalarcon@live.com>
2020-09-08 21:33:32 +0800

    update settings.component.ts
    - imported blockchain service
    - added blockchain variable
    - instantiated the blockchain variable

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import blockchain service
+import { BlockchainService } from 'src/app/services/blockchain.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  //declare a variable
+  public blockchain;
+  //instantiate blockchain
+  constructor(private blockchainService: BlockchainService) { 
+    this.blockchain = blockchainService.blockchainInstance;
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
