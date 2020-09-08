596cb2df355fdab275b2cf47a540da262e92a919
KmKowdz <kmalarcon@live.com>
2020-09-08 21:28:07 +0800

    create settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 import { BlockViewComponent } from './components/block-view/block-view.component';
 import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockchainViewerComponent,
     BlockViewComponent,
-    TransactionsTableComponent
+    TransactionsTableComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
