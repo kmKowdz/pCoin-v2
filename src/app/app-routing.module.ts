c6a3dc2de1a996c9b0f2735e504bfe104609dd08
KmKowdz <kmalarcon@live.com>
2020-09-08 22:10:01 +0800

    update app-routing.module.ts
    - imported the createtransaction component
    - added a path to where the create transaction component will be located

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 //import settings component
 import { SettingsComponent } from './pages/settings/settings.component';
+//import create transaction component
+import { CreateTransactionComponent } from './pages/create-transaction/create-transaction.component';
 
 const routes: Routes = [
   { path: '', component: BlockchainViewerComponent }, //add path where the component will be located
-  { path: 'settings', component: SettingsComponent } //add part to where the settings component will be found
+  { path: 'settings', component: SettingsComponent }, //add path to where the settings component will be found
+  { path: 'new/transaction', component: CreateTransactionComponent } //add path to where the create transaction component will be located
 ];
 
 @NgModule({
