21afdd3ecdbff7f18db5c8506281553ec170c2d4
KmKowdz <kmalarcon@live.com>
2020-09-08 21:42:28 +0800

    update app-routing.module.ts
    - imported settings component
    - added path to the settings component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //import the blockchain viewer component
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+//import settings component
+import { SettingsComponent } from './pages/settings/settings.component';
 
 const routes: Routes = [
-  { path: '', component: BlockchainViewerComponent } //add path where the component will be located
+  { path: '', component: BlockchainViewerComponent }, //add path where the component will be located
+  { path: 'settings', component: SettingsComponent } //add part to where the settings component will be found
 ];
 
 @NgModule({
