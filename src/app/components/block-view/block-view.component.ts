1a852c5a72cc0e3c20c7ad0881c3cd72ed50e891
KmKowdz <kmalarcon@live.com>
2020-09-08 19:46:17 +0800

    update block-view.component.ts
    - import blockchain service
    - declared a variable to store the input
    - added a method to get transaction's length
    - added method to return the block if selected
    - added a method to get the block number

--- src/app/components/block-view/block-view.component.ts
+++ src/app/components/block-view/block-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+//from savjee's source code
+//import blockchain service
+import { BlockchainService } from '../../services/blockchain.service';
 
 @Component({
   selector: 'app-block-view',
@@ -10,9 +13,34 @@ export class BlockViewComponent implements OnInit {
   //get the input and store it in a variable called block
   @Input() public block;
 
-  constructor() { }
+  //from savjee's source code
+  //to store the info from the input
+  @Input()
+  public selectedBlock;
+
+  private blocksInChain;
+
+  constructor(private blockchainService: BlockchainService) {
+    this.blocksInChain = blockchainService.blockchainInstance.chain;
+  }
 
   ngOnInit() {
+  }
+
+  //from savjee's source code
+  //to get transaction's length
+  blockHasTx() {
+    return this.block.transactions.length > 0;
+  }
+
+  //method to return the block if selected
+  isSelectedBlock() {
+    return this.block === this.selectedBlock;
+  }
+
+  //method to get the block number
+  getBlockNumber() {
+    return this.blocksInChain.indexOf(this.block) + 1;
   }
 
 }
