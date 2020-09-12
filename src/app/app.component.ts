ad5e154214dd4980afad02c2637d30db2c3e853c
KmKowdz <kmalarcon@live.com>
2020-09-12 14:10:49 +0800

    update app.component.ts
    - imported blockchain service
    - declared variables
    - instantiated blockchain service
    - added a method to dismiss the info message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+//import blockchain service
+import { BlockchainService } from './services/blockchain.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pCoin-v2';
+
+export class AppComponent implements OnInit {
+
+  //declare variables
+  public blockchain;
+  public showInfoMessage = true;
+
+  //instantiate blockchain service
+  constructor(private blockchainService: BlockchainService) {
+    this.blockchain = blockchainService.blockchainInstance;
+  }
+
+  ngOnInit() {
+  }
+
+  //add a method to dismiss the info message
+  dismissInfoMessage() {
+    this.showInfoMessage = false;
+  }
 }
