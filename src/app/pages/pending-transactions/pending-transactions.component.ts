086094654da0a6e972ee2f82fdc75a63a60610b1
KmKowdz <kmalarcon@live.com>
2020-09-12 15:57:59 +0800

    update pending-transactions.component.ts

--- src/app/pages/pending-transactions/pending-transactions.component.ts
+++ src/app/pages/pending-transactions/pending-transactions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import ActivatedRoute
+import { Router, ActivatedRoute } from '@angular/router';
 //import blockchain service
 import { BlockchainService } from 'src/app/services/blockchain.service';
 
@@ -8,21 +10,30 @@ import { BlockchainService } from 'src/app/services/blockchain.service';
   styleUrls: ['./pending-transactions.component.scss']
 })
 export class PendingTransactionsComponent implements OnInit {
-
   //declare a pending transactions variable
   public pendingTransactions = [];
+  public miningInProgress = false;
+  public justAddedTx = false;
 
   //inject blockchain services
-  constructor(private blockchainService: BlockchainService) {
+  constructor(private blockchainService: BlockchainService, private router: Router, private route: ActivatedRoute) {
     this.pendingTransactions = blockchainService.getPendingTransactions();
-   }
+  }
 
   ngOnInit() {
+    if (this.route.snapshot.paramMap.get('addedTx')) {
+      this.justAddedTx = true;
+
+      setTimeout(() => {
+        this.justAddedTx = false;
+      }, 4000);
+    }
   }
 
-  //add a method to mine the pending transactions
-  minePendingTransactions(){
+  minePendingTransactions() {
+    this.miningInProgress = true;
     this.blockchainService.minePendingTransactions();
+    this.miningInProgress = false;
+    this.router.navigate(['/']);
   }
-
 }
