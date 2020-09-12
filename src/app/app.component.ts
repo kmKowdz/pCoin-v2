e42f7c044f3c17861f536e08997307442e838a39
KmKowdz <kmalarcon@live.com>
2020-09-12 14:12:29 +0800

    update app.component.ts
    - resolved problem on the undeclared OnInit variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 //import blockchain service
 import { BlockchainService } from './services/blockchain.service';
 
